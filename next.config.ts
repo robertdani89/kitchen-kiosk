9963559032ef9dfdab2989948047626aff4bb05d
Robi <robertdani89@gmail.com>
2026-06-11 09:38:28 +0200

    first changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  allowedDevOrigins: ['192.168.0.182'],
 };
 
 export default nextConfig;
